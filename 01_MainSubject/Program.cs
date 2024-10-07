fc595689176a2c5da6814a328da59d41d5dc1ce0
Emircan Bağdu <emicanbagdu52@gmail.com>
2024-10-07 11:36:17 +0300

    Add project files.

---
+++ 01_MainSubject/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubject
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Yazdırma Komutları
+			//Console.WriteLine("Selam");
+			//Console.Write("Merhaba Dünya");
+			//Console.Read();
+			#endregion
+
+			#region StringDeğişkenler
+
+			//// Değişken_türü değişken_adı;
+
+			//string customerName, customerSurname, customerPhone, customerEmail, district, city;
+			//customerName = "Emircan";
+			//customerSurname = "Bagdu";
+			//customerPhone = "0555 555 5555";
+			//customerEmail = "emircanbagdu52@gamil.com";
+			//district = "Sancaktepe";
+			//city = "İstanbul";
+
+			//Console.WriteLine("*****************    Rezarvasyon Kartı    *****************");
+			//Console.WriteLine("");
+			//Console.WriteLine("-----------------------------------------------------------");
+			//Console.WriteLine("Müşteri :" + customerName + " " + customerSurname);
+			//Console.WriteLine("İletişim : " + customerPhone);
+			//Console.WriteLine("Email adresi : " + customerPhone);
+			//Console.WriteLine("Adres: " + district + " / " + city);
+			//Console.WriteLine("-----------------------------------------------------------");
+			//Console.Read();
+
+			#endregion
+
+			#region int Değişkenler
+
+			int number = 22;
+			Console.WriteLine(number);
+			Console.Read();
+
+			#endregion
+
+
+
+		}
+	}
+}
+//Yazdırma Komutları
---
+++ 02_Veriables/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Veriables
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region double değişkenler
+			////ondalıklı sayılar için kullanılır.
+
+			//double number;
+			//number = 7.75;
+			//Console.WriteLine(number);//Ekrana yazdırma 
+
+
+			#endregion
+
+			#region Char Değişkenler
+			////Karakter tutmak için kullanılır.
+			//char symbol= 'A';
+			//Console.WriteLine(symbol);
+			#endregion
+
+			#region Klavyeden veri girişleri 
+
+			Console.WriteLine("*******CSharp Hava Yolları Yolcu Bilgisi *******");
+			Console.WriteLine();
+			string passsengerName, passengerSurname;
+			int price;
+			Console.Write("Yolcu Adı : ");
+			passsengerName =Console.ReadLine();
+
+			Console.Write("Yolcu Soyadı : ");
+			passengerSurname =Console.ReadLine();
+
+			Console.Write("Fiyat");
+			price = int.Parse(Console.ReadLine());//girilen sayıyı int türüne çevirme double olursa double char olursa char yazılır.
+
+			Console.WriteLine();
+			Console.WriteLine("Yolcu: "+passsengerName+" "+passengerSurname);
+			Console.WriteLine("Fiyat:"+price);
+
+			#endregion
+
+			Console.Read();//Herhangi bir tuşa basana kadar console kapanmaz.
+		}
+	}
+}
