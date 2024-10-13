91a6ae02bbee8d87529836ea738b29913bffd36f
Emircan Bağdu <emicanbagdu52@gmail.com>
2024-10-13 21:01:48 +0300

    Loops

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region For Döngüsü
+
+			//For (x;y;z)
+			//x:başlangıç
+			//y:bitiş
+			//z:artış/azalış
+
+			//for (int i = 0; i < 5; i++)
+			//{
+			//	Console.WriteLine("Merhaba Dünya");
+			//}
+
+			#endregion
+
+			#region For Döngüsü İle Karar Yapıları
+
+			//for (int i = 0; i <= 100; i++)
+			//{
+			//	if (i%5==0)
+			//	{
+			//		Console.WriteLine(i);
+			//	}
+			//}
+
+			#endregion
+
+			#region While Döngüsü
+
+			//while(Şart)
+			//{ işlemler}
+
+			//int i = 1;
+			//while (i<=5)
+			//{
+			//	Console.WriteLine(i);
+			//	i++;
+			//}
+			#endregion
+
+			Console.ReadLine();
+		}
+	}
+}
