09f1e06c1ce3c7f9517a0cee69c4541e61cf3f3f
Emircan Bağdu <emicanbagdu52@gmail.com>
2024-10-14 19:12:23 +0300

    Arrays

---
+++ 06_Arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Arrays
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Temel Dizi Örnekleri
+			////2.4.6.8
+			////sarı,lacivert,kırmızı,beyaz
+			////DeğişkenTürü [] DiziAdı=new DeğişkenTürü[ElemanSayısı]
+			////DeğişkenTürü [] DiziAdı={"dizinin1.elemanı","dizinin2.elemanı"}
+			//string[] colors = new string[4];
+			//colors[0] = "sarı";
+			//colors[1] = "lacivert";
+			//colors[2] = "kırmızı";
+			//colors[3] = "beyaz";
+			//Console.WriteLine(colors[1]);
+			#endregion
+
+			#region Dizilerdeki Tüm Elemanları Listeleme
+
+			//string[] colors = { "Sarı", "Lacivert", "Mavi", "Kırmızı", "Beyaz" };
+
+			//for (int i = 0; i < colors.Length; i++)
+			//{
+			//	Console.WriteLine(colors[i]);
+			//}
+
+			#endregion
+
+			#region Dizi Örnekler
+			
+			int[] myArray = { 11, 3, 25, 37, 78, 96, 100, 888, 573, 1, 3, 7 };
+			int maxNumber = myArray[0];
+			for (int i = 0; i < myArray.Length; i++)
+			{
+				if (myArray[i] > maxNumber)
+				{
+					maxNumber = myArray[i];
+					
+				}
+			
+
+			}
+			Console.WriteLine(maxNumber);
+
+
+
+			#endregion
+
+			Console.ReadLine();
+		}
+	}
+}
