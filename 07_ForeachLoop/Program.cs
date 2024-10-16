51abc85a827b59bdf370aa788151a68758717efd
Emircan Bağdu <emicanbagdu52@gmail.com>
2024-10-16 17:48:15 +0300

    ForeachLoops

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ForeachLoop
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Foreach Döngüsü
+
+			//Foreach(1;2;3;4)
+			//1:Değişken türü
+			//2:Değişken Adı
+			//3:In
+			//4:Liste, Koleksiyon, Dizi
+			//for ile yapılan her şey foreach ile de yapılabilir. 
+
+
+
+
+			//string[] cities = { "İstanbul", "Ankara", "İzmir" };
+			//foreach (string city in cities)
+			//{
+			//	Console.WriteLine(city);
+			//}
+
+
+
+			//int toplam = 0;
+			//int[] number = { 11, 12, 25, 26, 35, 25, 87 };
+			//foreach (int i in number)
+			//{
+			//	toplam = toplam + i;
+			//}
+			//Console.WriteLine(toplam);
+
+
+			#endregion
+
+
+			#region Listeler
+
+			//List<int> nembers = new List<int>
+			//{ 1,2,3,4,5,6};
+
+			//foreach (int i in nembers)
+			//{
+			//	Console.WriteLine(i);
+			//}
+
+			#endregion
+
+			Console.ReadLine();
+		}
+	}
+}
